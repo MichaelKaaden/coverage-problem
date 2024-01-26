09dd08356626d5f0814d80a906231781c29d8e14
Michael Kaaden <github@kaaden.net>
2024-01-26 07:29:03 +0100

    Make sure code coverage is generated when running the unit tests

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,12 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+const globalCoverageConfiguration = {
+  collectCoverage: true,
+  coverageDirectory: `${process.env.NX_WORKSPACE_ROOT}/coverage/${process.env['NX_TASK_TARGET_PROJECT']}`,
+  coverageReporters: ['cobertura']
+};
+
+module.exports = {
+  ...nxPreset,
+  ...globalCoverageConfiguration
+};
